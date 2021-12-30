7a0a1565a323a5ab99d58be9f3e644e1670d3008
jaymesn <jjclyke@gmail.com>
2021-12-30 11:28:08 -0500

    inital linking to local commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let name: string;
+	
+</script>
+
+<main>
+	<p>Hello .</p>
+</main>
+
+<style>
+	:root{
+		--size:min(30vh,30vw);
+	}
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	}
+	p {
+		font-size:var(--size)
+	}
+
+
+
+</style>
