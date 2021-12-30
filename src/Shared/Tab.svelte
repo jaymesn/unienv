2f0a7b36c7456a0eb311ca63a3438e4e18c0f8e0
jaymesn <jjclyke@gmail.com>
2021-12-30 12:15:54 -0500

    separated shared components from main ones

---
+++ src/Shared/Tab.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { SvelteComponent } from "svelte/internal";
+    export let title:string;
+    export let icon:string;
+    export let component:any;
+</script>
+
+<main>
+
+</main>
+
+<style>
+
+</style>
