214dd1ae796df8e9696f9868ce9b96330ee2c5db
jaymesn <jjclyke@gmail.com>
2022-01-06 10:03:57 -0500

    finished refactoring

--- src/@types/Types.svelte
+++ src/@types/Types.svelte
@@ -14,13 +14,28 @@
         12?:{1?:CalendarDayReminders,2?:CalendarDayReminders,3?:CalendarDayReminders,4?:CalendarDayReminders,5?:CalendarDayReminders,6?:CalendarDayReminders,7?:CalendarDayReminders,8?:CalendarDayReminders,9?:CalendarDayReminders,10?:CalendarDayReminders,11?:CalendarDayReminders,12?:CalendarDayReminders,13?:CalendarDayReminders,14?:CalendarDayReminders,15?:CalendarDayReminders,16?:CalendarDayReminders,17?:CalendarDayReminders,18?:CalendarDayReminders,19?:CalendarDayReminders,20?:CalendarDayReminders,21?:CalendarDayReminders,22?:CalendarDayReminders,23?:CalendarDayReminders,24?:CalendarDayReminders,25?:CalendarDayReminders,26?:CalendarDayReminders,27?:CalendarDayReminders,28?:CalendarDayReminders,29?:CalendarDayReminders,30?:CalendarDayReminders,31?:CalendarDayReminders} 
     }
 
-    export interface WorkspaceData {
+    export type WorkspaceData = Map<workspaceName,Workspace>
+
+    /*
+    this is where u take ur thought graphs and add formatting
+    and other forms of content IE panels,imgs,iframes
+    */
+    interface Workspace {
 
     }
 
-    export interface ThoughtGraph {
+    /*
+    this should a TEXT only component where 
+    yes you can nest text into other text 
+    but the formatting is done in a different component
+    this in a sense is just a Template or interface that you 
+    layout the structure of what your thinking about */
+    interface ThoughtGraph {
 
     }
+
+
+    type CalendarDayReminders = Map<workspaceName,Array<Reminder>>; 
 
     type workspaceName = string;
 
@@ -30,7 +45,6 @@
         icon:string,
     };
 
-    type CalendarDayReminders = Map<workspaceName,Array<Reminder>>; 
     export interface NavItem { icon:string, title:string }
 
     export type Data = {
--- src/TabComponents/Calendar/Calendar.svelte
+++ src/TabComponents/Calendar/Calendar.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
+
+    import type { Dates } from "../../@types/Types.svelte";
+
     console.log("from Calendar")
+    export let allReminders:Dates;
+
 </script>
 
 <div class="Calendar">
 
-    <button>calendar</button>
 </div>
+<!--<CalendarLegend />-->
+
 <style>
-    .Calendar{
-        height: inherit;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    
+    .Calendar {
+        width:100%;
+        height:100%;
         background-color: blue;
+        display:grid;
     }
 </style>
